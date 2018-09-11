62985b2af547c9c969000647cb63802d53f78e1c
Lui de la Parra <lui@mutesymphony.com>
2018-09-11 08:38:00 -0700

    Adds Script Load Test
    This commit adds a script load test.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 module.exports = function(RED) {
-    function script(config) {
-        RED.nodes.createNode(this, config);
-        const node = this;
-        node.connection = RED.nodes.getNode(config.client);
-        node.on("input", msg =>
-            this.connection.client
-                .script(config.script, config.layout, msg)
-                .then(response => node.send(response))
-                .catch(error => { console.log(error) ; node.error("script error", error)})
-        );
-    }
-    RED.nodes.registerType("trigger-script", script);
+  function script(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    node.connection = RED.nodes.getNode(config.client);
+    node.on("input", msg =>
+      this.connection.client
+        .script(config.script, config.layout, msg)
+        .then(response => node.send(response))
+        .catch(error => node.error("script error", error))
+    );
+  }
+  RED.nodes.registerType("trigger-script", script);
 };
---
+++ test/script_spec.js
@@ -0,0 +1,24 @@
+/* global describe beforeEach afterEach it */
+
+const helper = require("node-red-node-test-helper");
+const script = require("../script.js");
+
+helper.init(require.resolve("node-red"));
+
+describe("Script Node", function() {
+  beforeEach(function(done) {
+    helper.startServer(done);
+  });
+
+  afterEach(function(done) {
+    helper.unload();
+    helper.stopServer(done);
+  });
+
+  it("should be loaded", function(done) {
+    const testFlows = [{ id: "n1", type: "inject" }];
+    helper.load(script, testFlows, function() {
+      done();
+    });
+  });
+});
