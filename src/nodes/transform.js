95e91d73a5c4f14c65f528f46cd193a746b56fdb
Lui de la Parra <lui@mutesymphony.com>
2018-10-13 13:02:52 -0700

    Adds Transform Documentation
    This commit adds transform node documentation.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- src/nodes/transform.js
+++ src/nodes/transform.js
@@ -1,11 +1,16 @@
 module.exports = function(RED) {
   function transform(config) {
     const { transform } = require("fms-api-client");
-    const { merge, parse } = require("../services");
+    const { merge, parse, compact } = require("../services");
     RED.nodes.createNode(this, config);
     const node = this;
     node.on("input", msg => {
-      const parameters = config.parameters || msg.parameters || {};
+      const parameters = compact([
+        config.parameters,
+        msg.parameters,
+        msg.payload.parameters,
+        {}
+      ]);
       node.send(
         merge(
           msg,
