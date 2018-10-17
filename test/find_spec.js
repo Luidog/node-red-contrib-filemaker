c57b80cb06ce00b7546052c4639858264c0c2d67
Lui de la Parra <lui@mutesymphony.com>
2018-10-16 18:21:06 -0700

    Adds Tests
    This commit adds tests to for the find node.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- test/find_spec.js
+++ test/find_spec.js
@@ -1,11 +1,21 @@
 /* global describe beforeEach afterEach it */
-
+const { expect } = require("chai");
 const helper = require("node-red-node-test-helper");
-const find = require("../src/nodes/find.js");
+const environment = require("dotenv");
+const varium = require("varium");
+const clientNode = require("../src/client/client.js");
+const findNode = require("../src/nodes/find.js");
+const catchNode = require("./core/25-catch.js");
 
 helper.init(require.resolve("node-red"));
 
-describe("Perform Find Node", function() {
+describe("Find Records Node", function() {
+  before(function(done) {
+    environment.config({ path: "./test/.env" });
+    varium(process.env, "./test/env.manifest");
+    done();
+  });
+
   beforeEach(function(done) {
     helper.startServer(done);
   });
@@ -16,9 +26,106 @@ describe("Perform Find Node", function() {
   });
 
   it("should be loaded", function(done) {
-    const testFlows = [{ id: "n1", type: "inject" }];
-    helper.load(find, testFlows, function() {
+    const testFlow = [{ id: "n1", type: "inject" }];
+    helper.load(findNode, testFlow, function() {
       done();
+    });
+  });
+
+  it("should perform a find", function(done) {
+    const testFlow = [
+      {
+        id: "3783b2da.4346a6",
+        type: "filemaker-api-client",
+        server: process.env.FILEMAKER_SERVER,
+        name: "Mute Symphony",
+        application: process.env.FILEMAKER_APPLICATION,
+        user: process.env.FILEMAKER_USERNAME,
+        password: process.env.FILEMAKER_PASSWORD,
+        usage: true
+      },
+      {
+        id: "n1",
+        type: "perform-find",
+        client: "3783b2da.4346a6",
+        layout: "People",
+        scripts: "",
+        merge: true,
+        wires: [["n2"]]
+      },
+      { id: "n2", type: "helper" }
+    ];
+    helper.load([clientNode, findNode], testFlow, function() {
+      const testNode = helper.getNode("n1");
+      const helperNode = helper.getNode("n2");
+      helperNode.on("input", function(msg) {
+        try {
+          expect(msg)
+            .to.be.an("object")
+            .with.any.keys("payload")
+            .and.property("payload")
+            .to.be.a("object")
+            .with.any.keys("data");
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+      testNode.receive({ payload: { query: { name: '*' } } });
+    });
+  });
+
+  it("should throw an error with a message and a code", function(done) {
+    const testFlow = [
+      {
+        id: "f1",
+        type: "tab",
+        label: "Catch Find Error"
+      },
+      {
+        id: "3783b2da.4346a6",
+        type: "filemaker-api-client",
+        server: process.env.FILEMAKER_SERVER,
+        name: "Mute Symphony",
+        z: "f1",
+        application: process.env.FILEMAKER_APPLICATION,
+        user: process.env.FILEMAKER_USERNAME,
+        password: process.env.FILEMAKER_PASSWORD,
+        usage: true
+      },
+      {
+        id: "n2",
+        type: "perform-find",
+        client: "3783b2da.4346a6",
+        layout: "People",
+        z: "f1",
+        scripts: "",
+        merge: true,
+        wires: [["n2"]]
+      },
+      {
+        id: "n1",
+        type: "catch",
+        z: "f1",
+        name: "catch",
+        wires: [["n3"]]
+      },
+      { id: "n3", type: "helper" }
+    ];
+    helper.load([clientNode, findNode, catchNode], testFlow, function() {
+      const testNode = helper.getNode("n2");
+      const helperNode = helper.getNode("n3");
+      helperNode.on("input", function(msg) {
+        try {
+          expect(msg)
+            .to.be.an("object")
+            .with.any.keys("_msgid", "code", "error", "code", "message");
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+      testNode.receive({ payload: { } });
     });
   });
 });
