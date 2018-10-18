bb8f269d2801a86d2133ba97586a5edd00c6edda
Lui de la Parra <lui@mutesymphony.com>
2018-10-18 14:15:07 -0700

    Fixes Async Function
    This commit fixes the async input event function.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- src/nodes/globals.js
+++ src/nodes/globals.js
@@ -5,9 +5,9 @@ module.exports = function(RED) {
     const node = this;
     const { client, ...configuration } = config;
     node.connection = RED.nodes.getNode(client);
-    node.on("input", msg => {
+    node.on("input", async msg => {
       const data = configuration.globals || msg.payload;
-      let connection = await this.connection.client
+      let connection = await this.connection.client;
       connection
         .globals(data)
         .then(response => node.send(merge(msg, response)))
