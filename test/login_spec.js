312617b7aca738645c76885ed1744cca505de7ae
Lui de la Parra <lui@mutesymphony.com>
2018-12-08 16:04:13 -0800

    Revises Authentication Tests
    This commit revises authentication tests to fully test the login and logout nodes. This commit brings the back to complete test coverage.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- test/login_spec.js
+++ test/login_spec.js
@@ -3,14 +3,13 @@ const { expect } = require("chai");
 const helper = require("node-red-node-test-helper");
 const environment = require("dotenv");
 const varium = require("varium");
-const logoutNode = require("../src/nodes/logout.js");
-const listNode = require("../src/nodes/list.js");
+const loginNode = require("../src/nodes/login.js");
 const clientNode = require("../src/client/client.js");
 const catchNode = require("./core/25-catch.js");
 
 helper.init(require.resolve("node-red"));
 
-describe("Log Out Node", function() {
+describe("Login Node", function() {
   before(function(done) {
     environment.config({ path: "./test/.env" });
     varium(process.env, "./test/env.manifest");
@@ -28,95 +27,27 @@ describe("Log Out Node", function() {
 
   it("should be loaded", function(done) {
     const testFlow = [{ id: "n1", type: "inject" }];
-    helper.load(logoutNode, testFlow, function() {
+    helper.load(loginNode, testFlow, function() {
       done();
     });
   });
-
-  it("should close a Data API Session", function(done) {
-    var testFlows = [
-      {
-        id: "f1",
-        type: "tab",
-        label: "Logout Test"
-      },
-      {
-        id: "3783b2da.4346a6",
-        type: "filemaker-api-client",
-        server: process.env.FILEMAKER_SERVER,
-        name: "Sweet FM Client",
-        application: process.env.FILEMAKER_APPLICATION,
-        usage: true
-      },
-      {
-        id: "n1",
-        type: "logout",
-        client: "3783b2da.4346a6",
-        wires: [["n3"]]
-      },
-      {
-        id: "n2",
-        type: "catch",
-        z: "f1",
-        name: "catch",
-        wires: [["n3"]]
-      },
-      { id: "n3", type: "helper" }
-    ];
-    helper.load(
-      [clientNode, logoutNode, catchNode],
-      testFlows,
-      {
-        "3783b2da.4346a6": {
-          username: process.env.FILEMAKER_USERNAME,
-          password: process.env.FILEMAKER_PASSWORD
-        }
-      },
-      function() {
-        const list = helper.getNode("n1");
-        const helperNode = helper.getNode("n3");
-        helperNode.on("input", function(msg) {
-          try {
-            expect(msg)
-              .to.be.an("object")
-              .with.any.keys("payload");
-            done();
-          } catch (err) {
-            done(err);
-          }
-        });
-        list.receive({ payload: {} });
-      }
-    );
-  });
-  it("should close a Data API Session", function(done) {
+  it("should login to a Data API session", function(done) {
     var testFlow = [
       {
         id: "f1",
         type: "tab",
-        label: "Logout Data API Session"
+        label: "Login To Data API Session"
+      },
+      {
+        id: "n1",
+        type: "login",
+        z: "f1",
+        name: "login Node",
+        client: "3783b2da.4346a6",
+        wires: [["n2"]]
       },
       {
         id: "n2",
-        type: "list-records",
-        z: "f1",
-        name: "List Node",
-        client: "3783b2da.4346a6",
-        layout: "Devices",
-        scripts: "",
-        merge: true,
-        wires: [["n3"]]
-      },
-      {
-        id: "n3",
-        type: "logout",
-        z: "f1",
-        name: "logout Node",
-        client: "3783b2da.4346a6",
-        wires: [["n4"]]
-      },
-      {
-        id: "n4",
         type: "helper",
         z: "f1"
       },
@@ -129,15 +60,15 @@ describe("Log Out Node", function() {
         usage: true
       },
       {
-        id: "n1",
+        id: "n3",
         type: "catch",
         z: "f1",
         name: "catch",
-        wires: [["n4"]]
+        wires: [["n2"]]
       }
     ];
     helper.load(
-      [clientNode, listNode, logoutNode, catchNode],
+      [clientNode, loginNode, catchNode],
       testFlow,
       {
         "3783b2da.4346a6": {
@@ -146,9 +77,74 @@ describe("Log Out Node", function() {
         }
       },
       function() {
-        const list = helper.getNode("n2");
- 
-        const helperNode = helper.getNode("n4");
+        const login = helper.getNode("n1");
+
+        const helperNode = helper.getNode("n2");
+        helperNode.on("input", function(msg) {
+          try {
+            expect(msg)
+              .to.be.an("object")
+              .with.any.keys("payload")
+              .and.property("payload")
+              .to.have.any.keys("token", "_msgid");
+            done();
+          } catch (err) {
+            done(err);
+          }
+        });
+        login.receive({ payload: {} });
+      }
+    );
+  });
+  it("should throw an error with a message and a code", function(done) {
+    var testFlow = [
+      {
+        id: "f1",
+        type: "tab",
+        label: "Login Error Test"
+      },
+      {
+        id: "n1",
+        type: "login",
+        z: "f1",
+        name: "login Node",
+        client: "3783b2da.4346a6",
+        wires: [["n2"]]
+      },
+      {
+        id: "n2",
+        type: "helper",
+        z: "f1"
+      },
+      {
+        id: "3783b2da.4346a6",
+        type: "filemaker-api-client",
+        server: process.env.FILEMAKER_SERVER,
+        name: "Mute Symphony",
+        application: process.env.FILEMAKER_APPLICATION,
+        usage: true
+      },
+      {
+        id: "n3",
+        type: "catch",
+        z: "f1",
+        name: "catch",
+        wires: [["n2"]]
+      }
+    ];
+    helper.load(
+      [clientNode, loginNode, catchNode],
+      testFlow,
+      {
+        "3783b2da.4346a6": {
+          username: process.env.FILEMAKER_USERNAME,
+          password: "wrong-password"
+        }
+      },
+      function() {
+        const login = helper.getNode("n1");
+
+        const helperNode = helper.getNode("n2");
         helperNode.on("input", function(msg) {
           try {
             expect(msg)
@@ -159,7 +155,7 @@ describe("Log Out Node", function() {
             done(err);
           }
         });
-        list.receive({ payload: { layout: "none" } });
+        login.receive({ payload: {} });
       }
     );
   });
--- test/logout_spec.js
+++ test/logout_spec.js
@@ -3,13 +3,14 @@ const { expect } = require("chai");
 const helper = require("node-red-node-test-helper");
 const environment = require("dotenv");
 const varium = require("varium");
+const logoutNode = require("../src/nodes/logout.js");
 const loginNode = require("../src/nodes/login.js");
 const clientNode = require("../src/client/client.js");
 const catchNode = require("./core/25-catch.js");
 
 helper.init(require.resolve("node-red"));
 
-describe("Login Node", function() {
+describe("Log Out Node", function() {
   before(function(done) {
     environment.config({ path: "./test/.env" });
     varium(process.env, "./test/env.manifest");
@@ -27,26 +28,23 @@ describe("Login Node", function() {
 
   it("should be loaded", function(done) {
     const testFlow = [{ id: "n1", type: "inject" }];
-    helper.load(loginNode, testFlow, function() {
+    helper.load(logoutNode, testFlow, function() {
       done();
     });
   });
-  it("should login to a Data API session", function(done) {
+  it("should close a Data API Session", function(done) {
     var testFlow = [
       {
         id: "f1",
         type: "tab",
-        label: "Login To Data API Session"
+        label: "Logout Data API Session"
       },
       {
         id: "n2",
-        type: "list-records",
+        type: "login",
         z: "f1",
-        name: "List Node",
+        name: "login Node",
         client: "3783b2da.4346a6",
-        layout: "Devices",
-        scripts: "",
-        merge: true,
         wires: [["n3"]]
       },
       {
@@ -79,7 +77,7 @@ describe("Login Node", function() {
       }
     ];
     helper.load(
-      [clientNode, loginNode, catchNode],
+      [clientNode, loginNode, logoutNode, catchNode],
       testFlow,
       {
         "3783b2da.4346a6": {
@@ -88,8 +86,7 @@ describe("Login Node", function() {
         }
       },
       function() {
-        const list = helper.getNode("n2");
-
+        const login = helper.getNode("n2");
         const helperNode = helper.getNode("n4");
         helperNode.on("input", function(msg) {
           try {
@@ -101,7 +98,66 @@ describe("Login Node", function() {
             done(err);
           }
         });
-        list.receive({ payload: { layout: "none" } });
+        login.receive({ payload: {} });
+      }
+    );
+  });
+  it("should throw an error with a message and a code", function(done) {
+    var testFlows = [
+      {
+        id: "f1",
+        type: "tab",
+        z: "f1",
+        label: "Logout Error Test"
+      },
+      {
+        id: "3783b2da.4346a6",
+        type: "filemaker-api-client",
+        server: process.env.FILEMAKER_SERVER,
+        name: "Sweet FM Client",
+        application: process.env.FILEMAKER_APPLICATION,
+        usage: true
+      },
+      {
+        id: "n1",
+        z: "f1",
+        type: "logout",
+        client: "3783b2da.4346a6",
+        wires: [["n3"]]
+      },
+      {
+        id: "n2",
+        type: "catch",
+        z: "f1",
+        name: "catch",
+        wires: [["n3"]]
+      },
+      { id: "n3",z: "f1", type: "helper" }
+    ];
+    helper.load(
+      [clientNode, logoutNode, catchNode],
+      testFlows,
+      {
+        "3783b2da.4346a6": {
+          username: process.env.FILEMAKER_USERNAME,
+          password: process.env.FILEMAKER_PASSWORD
+        }
+      },
+      function() {
+        const logout = helper.getNode("n1");
+        const helperNode = helper.getNode("n3");
+        helperNode.on("input", function(msg) {
+          console.log(msg)
+          try {
+            expect(msg)
+              .to.be.an("object")
+              .with.any.keys("_msgid", "code", "error", "message");
+            done();
+          } catch (err) {
+            done(err);
+          }
+        });
+        logout.receive({ payload: {message:true} });
       }
     );
   });
