94a95c467801ba59ac87ed651a6682095d1cc0af
Lui de la Parra <lui@mutesymphony.com>
2018-12-18 19:23:37 -0800

    Adds Create Test
    This commit updates create tests to test additional parameters.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- test/create_spec.js
+++ test/create_spec.js
@@ -112,6 +112,86 @@ describe("Create Record Node", function() {
       }
     );
   });
+  it("should create allow the filemaker response to be merged to the message object", function(done) {
+    var testFlows = [
+      {
+        id: "ec096890.cdd65",
+        type: "tab",
+        label: "Create Record",
+        disabled: false,
+        info: ""
+      },
+      {
+        id: "369e311d.23d2de",
+        type: "helper"
+      },
+      {
+        id: "d3becaad.b78ce",
+        type: "catch",
+        z: "ec096890.cdd65",
+        name: "",
+        scope: null,
+        x: 360,
+        y: 100,
+        wires: [["369e311d.23d2de"]]
+      },
+      {
+        id: "db596a45.2ca398",
+        type: "create-record",
+        z: "ec096890.cdd65",
+        client: "e5173483.adc92",
+        layout: "payload.layout",
+        layoutType: "msg",
+        data: "",
+        dataType: "none",
+        scripts: "",
+        scriptsType: "none",
+        merge: "true",
+        mergeType: "bool",
+        output: "payload",
+        x: 340,
+        y: 40,
+        wires: [["369e311d.23d2de"]]
+      },
+      {
+        id: "e5173483.adc92",
+        type: "filemaker-api-client",
+        z: "",
+        server: process.env.FILEMAKER_SERVER,
+        name: "Mute Symphony",
+        application: process.env.FILEMAKER_APPLICATION,
+        usage: true
+      }
+    ];
+    helper.load(
+      [clientNode, createNode, catchNode],
+      testFlows,
+      {
+        "e5173483.adc92": {
+          username: process.env.FILEMAKER_USERNAME,
+          password: process.env.FILEMAKER_PASSWORD
+        }
+      },
+      function() {
+        const createNode = helper.getNode("db596a45.2ca398");
+        const helperNode = helper.getNode("369e311d.23d2de");
+        helperNode.on("input", function(msg) {
+          try {
+            expect(msg)
+              .to.be.an("object")
+              .with.any.keys("payload")
+              .and.property("payload")
+              .to.be.a("object")
+              .with.any.keys("recordId", "modId");
+            done();
+          } catch (err) {
+            done(err);
+          }
+        });
+        createNode.receive({ payload: { layout: "people" } });
+      }
+    );
+  });
   it("should use flow context to create a record.", function(done) {
     var testFlows = [
       {
