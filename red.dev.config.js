e06a190c71ff9126a48397f40171426260fe5902
Lui de la Parra <lui@mutesymphony.com>
2019-01-14 17:25:44 -0800

    Updates Development Configuration
    This commit updates the development configuration for the project to remove hardcoded process values and clean up project development data.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- red.dev.config.js
+++ red.dev.config.js
@@ -83,7 +83,7 @@ module.exports = {
 
   // By default, all user data is stored in the Node-RED install directory. To
   // use a different location, the following property can be used
-  userDir: path.join(__dirname, "./examples"),
+  userDir: path.join(__dirname, "./user"),
 
   // Node-RED scans the `nodes` directory in the install directory to find nodes.
   // The following property can be used to specify an additional directory to scan.
