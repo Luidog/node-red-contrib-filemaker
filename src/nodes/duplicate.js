28c59296a9e882168c84ca29b497879d8e378103
Lui de la Parra <Lui@mutesymphony.com>
2019-06-29 09:48:14 -0700

    Adds Duplicate Node
    This commit adds a record duplication node to the project.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

---
+++ src/nodes/duplicate.js
@@ -0,0 +1,26 @@
+module.exports = function(RED) {
+  function duplicate(config) {
+    const { merge, constructParameters } = require("../services");
+    RED.nodes.createNode(this, config);
+    const node = this;
+    const { client, ...configuration } = config;
+    const context = node.context();
+    node.connection = RED.nodes.getNode(client);
+    node.on("input", async message => {
+      let { layout, recordId, ...parameters } = constructParameters(
+        message,
+        configuration,
+        node.context(),
+        ["layout", "recordId", "scripts", "portals"]
+      );
+      let connection = await this.connection.client;
+      connection
+        .duplicate(layout, recordId, parameters)
+        .then(response =>
+          node.send(merge(configuration.output, message, response))
+        )
+        .catch(error => node.error(error.message, message));
+    });
+  }
+  RED.nodes.registerType("dapi-duplicate", duplicate);
+};
