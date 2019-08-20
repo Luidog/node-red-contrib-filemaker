1f6f309975cb7887ac45b31a3ec6bededcc5eb7c
luidog <lui@mutesymphony.com>
2019-08-20 07:06:50 -0700

    Updates Create Node
    This commit updates the create node to use the new send and handleError utility nodes and set status visuals.

--- src/nodes/create.js
+++ src/nodes/create.js
@@ -1,24 +1,30 @@
 module.exports = function(RED) {
   function create(config) {
-    const { merge, constructParameters, castBooleans } = require("../services");
+    const {
+      constructParameters,
+      castBooleans,
+      send,
+      handleError
+    } = require("../services");
     RED.nodes.createNode(this, config);
     const node = this;
     const { client, ...configuration } = config;
+    this.status({ fill: "green", shape: "dot", text: "Ready" });
     node.connection = RED.nodes.getNode(client);
     node.on("input", async message => {
+      this.status({ fill: "yellow", shape: "dot", text: "Processing" });
       const { layout, data, ...parameters } = constructParameters(
         message,
         configuration,
         node.context(),
         ["layout", "scripts", "data", "merge"]
       );
+      const { output } = configuration;
       const connection = await this.connection.client;
       connection
         .create(layout, data || {}, castBooleans(parameters))
-        .then(response =>
-          node.send(merge(configuration.output, message, response))
-        )
-        .catch(error => node.error(error.message, message));
+        .then(response => send(node, output, message, response))
+        .catch(error => handleError(node, error.message, message));
     });
   }
   RED.nodes.registerType("dapi-create-record", create);
