796e451f50572658a511655107cb4bbb9baf83c9
Lui de la Parra <Lui@mutesymphony.com>
2019-08-22 07:17:40 -0600

    Updates ProductInfo Node
    This commit updates the productInfo node to use the new send and handleError utility nodes and set status visuals.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/nodes/productInfo.js
+++ src/nodes/productInfo.js
@@ -1,18 +1,17 @@
 module.exports = function(RED) {
   function productInfo(configuration) {
-    const { merge } = require("../services");
+    const { send, handleError } = require("../services");
     RED.nodes.createNode(this, configuration);
     const node = this;
     const { client } = configuration;
     node.connection = RED.nodes.getNode(client);
     node.on("input", async message => {
-      const client = await this.connection.client;
-      client
+      this.status({ fill: "yellow", shape: "dot", text: "Processing" });
+      const connection = await this.connection.client;
+      connection
         .productInfo()
-        .then(response =>
-          node.send(merge(configuration.output, message, response))
-        )
-        .catch(error => node.error(error.message, message));
+        .then(response => send(node, output, message, response))
+        .catch(error => handleError(node, error.message, message));
     });
   }
   RED.nodes.registerType("dapi-product-info", productInfo);
