2235a924590a0000dae02f448c40ca77f22b339b
Lui de la Parra <Lui@mutesymphony.com>
2019-08-22 07:18:15 -0600

    Updates Transform Node
    This commit updates the transform node to use the new send and handleError utility nodes and set status visuals.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/nodes/transform.js
+++ src/nodes/transform.js
@@ -1,10 +1,12 @@
 module.exports = function(RED) {
   function transform(configuration) {
     const { transform } = require("fms-api-client");
-    const { merge, isJson, constructParameters } = require("../services");
+    const { send, handleError, isJson, constructParameters } = require("../services");
     RED.nodes.createNode(this, configuration);
     const node = this;
+    this.status({ fill: "green", shape: "dot", text: "Ready" });
     node.on("input", message => {
+      this.status({ fill: "yellow", shape: "dot", text: "Processing" });
       const { data, ...parameters } = constructParameters(
         message,
         configuration,
@@ -13,14 +15,12 @@ module.exports = function(RED) {
       );
       try {
         if (isJson(data)) {
-          node.send(
-            merge(configuration.output, message, transform(data, parameters))
-          );
+          send(node, output, message, response)
         } else {
           throw Error("data must be valid json");
         }
       } catch (error) {
-        node.error(error.message, message);
+        handleError(node, error.message, message);
       }
     });
   }
