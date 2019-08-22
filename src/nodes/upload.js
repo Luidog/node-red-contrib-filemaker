74a55bbbff85622e44f7d51e2b74f5765cfb0453
Lui de la Parra <Lui@mutesymphony.com>
2019-08-22 07:12:25 -0600

    Revises Upload Nodes
    This commit revises the upload node to use the new send and handleError utility nodes and set status visuals.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/nodes/upload.js
+++ src/nodes/upload.js
@@ -1,11 +1,13 @@
 module.exports = function(RED) {
   function upload(config) {
-    const { merge, constructParameters } = require("../services");
+    const { send, handleError, constructParameters } = require("../services");
     RED.nodes.createNode(this, config);
     const node = this;
+    this.status({ fill: "green", shape: "dot", text: "Ready" });
     const { client, ...configuration } = config;
     node.connection = RED.nodes.getNode(client);
     node.on("input", async message => {
+      this.status({ fill: "yellow", shape: "dot", text: "Processing" });
       const {
         layout,
         field,
@@ -22,10 +24,8 @@ module.exports = function(RED) {
       const connection = await this.connection.client;
       connection
         .upload(file, layout, field, recordId, parameters)
-        .then(response =>
-          node.send(merge(configuration.output, message, response))
-        )
-        .catch(error => node.error(error.message, message));
+        .then(response => send(node, output, message, response))
+        .catch(error => handleError(node, error.message, message));
     });
   }
   RED.nodes.registerType("dapi-upload-file", upload);
