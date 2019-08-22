3f239207d4a2449e964ca0b9648fe8605b20c767
Lui de la Parra <Lui@mutesymphony.com>
2019-08-22 06:39:25 -0600

    Updates Delete Node
    This commit updates the delete node to use the new send and handleError utility nodes and set status visuals.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/nodes/delete.js
+++ src/nodes/delete.js
@@ -1,24 +1,24 @@
 module.exports = function(RED) {
   function deleteRecords(config) {
-    const { merge, constructParameters } = require("../services");
+    const { send, handleError, constructParameters } = require("../services");
     RED.nodes.createNode(this, config);
     const node = this;
-    const { client, ...configuration } = config;
+    this.status({ fill: "green", shape: "dot", text: "Ready" });
+    const { client, output, ...configuration } = config;
     node.connection = RED.nodes.getNode(client);
     node.on("input", async message => {
+      this.status({ fill: "yellow", shape: "dot", text: "Processing" });
       const { layout, recordId, ...parameters } = constructParameters(
         message,
         configuration,
         node.context(),
         ["layout", "scripts", "recordId"]
       );
-      const client = await this.connection.client;
-      client
+      const connection = await this.connection.client;
+      connection
         .delete(layout, recordId, parameters)
-        .then(response =>
-          node.send(merge(configuration.output, message, response))
-        )
-        .catch(error => node.error(error.message, message));
+        .then(response => send(node, output, message, response))
+        .catch(error => handleError(node, error.message, message));
     });
   }
   RED.nodes.registerType("dapi-delete-record", deleteRecords);
