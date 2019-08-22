ae170fc941c1f58db3f039a66c628d0c6b34990f
Lui de la Parra <Lui@mutesymphony.com>
2019-08-22 07:17:05 -0600

    Updates Layout Node
    This commit updates the layout node to use the new send and handleError utility nodes and set status visuals.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/nodes/layout.js
+++ src/nodes/layout.js
@@ -1,24 +1,24 @@
 module.exports = function(RED) {
   function layout(configuration) {
-    const { merge, constructParameters } = require("../services");
+    const { send, handleError, constructParameters } = require("../services");
     RED.nodes.createNode(this, configuration);
     const node = this;
+    this.status({ fill: "green", shape: "dot", text: "Ready" });
     const { client } = configuration;
     node.connection = RED.nodes.getNode(client);
     node.on("input", async message => {
+      this.status({ fill: "yellow", shape: "dot", text: "Processing" });
       const { layout } = constructParameters(
         message,
         configuration,
         node.context(),
         ["layout"]
       );
-      const client = await this.connection.client;
-      client
+      const connection = await this.connection.client;
+      connection
         .layout(layout)
-        .then(response =>
-          node.send(merge(configuration.output, message, response))
-        )
-        .catch(error => node.error(error.message, message));
+        .then(response => send(node, output, message, response))
+        .catch(error => handleError(node, error.message, message));
     });
   }
   RED.nodes.registerType("dapi-layout", layout);
