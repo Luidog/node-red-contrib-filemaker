be2f5a22e1cc610f915d4e0347e40c4295e00861
Lui de la Parra <Lui@mutesymphony.com>
2019-08-22 06:39:06 -0600

    Updates Edit Node
    This commit updates the Edit node to use the new send and handleError utility nodes and set status visuals.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/nodes/edit.js
+++ src/nodes/edit.js
@@ -1,8 +1,9 @@
 module.exports = function(RED) {
   function edit(config) {
-    const { merge, constructParameters, castBooleans } = require("../services");
+    const { send, handleError, constructParameters, castBooleans } = require("../services");
     RED.nodes.createNode(this, config);
     const node = this;
+    this.status({ fill: "green", shape: "dot", text: "Ready" });
     const { client, ...configuration } = config;
     node.connection = RED.nodes.getNode(client);
     node.on("input", async message => {
@@ -15,10 +16,8 @@ module.exports = function(RED) {
       const connection = await this.connection.client;
       connection
         .edit(layout, recordId, data, castBooleans(parameters))
-        .then(response =>
-          node.send(merge(configuration.output, message, response))
-        )
-        .catch(error => node.error(error.message, message));
+        .then(response => send(node, output, message, response))
+        .catch(error => handleError(node, error.message, message));
     });
   }
   RED.nodes.registerType("dapi-edit-record", edit);
