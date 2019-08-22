6f244988e9c42dbc2a4aa83df098d2e42ad80587
Lui de la Parra <Lui@mutesymphony.com>
2019-08-22 07:18:40 -0600

    Updates Globals Node
    This commit updates the globals node to use the new send and handleError utility nodes and set status visuals.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/nodes/globals.js
+++ src/nodes/globals.js
@@ -3,9 +3,11 @@ module.exports = function(RED) {
     const { merge, constructParameters } = require("../services");
     RED.nodes.createNode(this, config);
     const node = this;
+    this.status({ fill: "green", shape: "dot", text: "Ready" });
     const { client, ...configuration } = config;
     node.connection = RED.nodes.getNode(client);
     node.on("input", async message => {
+      this.status({ fill: "yellow", shape: "dot", text: "Processing" });
       const connection = await this.connection.client;
       const { data } = constructParameters(
         message,
@@ -15,10 +17,8 @@ module.exports = function(RED) {
       );
       connection
         .globals(data)
-        .then(response =>
-          node.send(merge(configuration.output, message, response))
-        )
-        .catch(error => node.error(error.message, message));
+        .then(response => send(node, output, message, response))
+        .catch(error => handleError(node, error.message, message));
     });
   }
   RED.nodes.registerType("dapi-set-globals", globals);
