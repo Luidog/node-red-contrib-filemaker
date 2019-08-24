0c887cca2d6242c2990e9b3cf5619543203cf53b
Lui de la Parra <Lui@mutesymphony.com>
2019-08-24 07:45:06 -0600

    Revises List Node
    This commit revises the list node to listen for connection status events.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/nodes/list.js
+++ src/nodes/list.js
@@ -1,13 +1,23 @@
 module.exports = function(RED) {
   function list(config) {
-    this.status({ fill: "green", shape: "dot", text: "Ready" });
     const { send, handleError, constructParameters } = require("../services");
+    const { client, output, ...configuration } = config;
     RED.nodes.createNode(this, config);
     const node = this;
-    const { client, ...configuration } = config;
+    node.status({ fill: "blue", shape: "dot", text: "Loading" });
+
+    node.handleEvent = ({ connected, message }) =>
+      node.status(
+        connected
+          ? { fill: "green", shape: "dot", text: message }
+          : { fill: "red", shape: "dot", text: message }
+      );
+    
     node.connection = RED.nodes.getNode(client);
+    node.connection.on("status", node.handleEvent);
+
     node.on("input", async message => {
-      this.status({ fill: "yellow", shape: "dot", text: "Processing" });
+      node.status({ fill: "yellow", shape: "dot", text: "Processing" });
       const { layout, ...parameters } = constructParameters(
         message,
         configuration,
@@ -15,11 +25,18 @@ module.exports = function(RED) {
         ["layout", "limit", "offset", "sort", "scripts", "portals"]
       );
 
-      const connection = await this.connection.client;
-      connection
+      const client = await node.connection.client;
+
+      client
         .list(layout, parameters)
         .then(response => send(node, output, message, response))
         .catch(error => handleError(node, error.message, message));
+    });
+
+    node.on("close", done => {
+      node.connection.removeListener("error", node.handleEvent);
+      node.connection.removeListener("status", node.handleEvent);
+      done();
     });
   }
   RED.nodes.registerType("dapi-list-records", list);
