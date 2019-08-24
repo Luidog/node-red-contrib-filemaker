ed4b85fc775935cf44cf2167f7387fcbbf615b48
Lui de la Parra <Lui@mutesymphony.com>
2019-08-24 08:14:08 -0600

    Revises Database Nodes
    This commit revises the database node to listen for status events and check to see if a client has properly loaded before using it.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/nodes/databases.js
+++ src/nodes/databases.js
@@ -1,11 +1,24 @@
 module.exports = function(RED) {
   function databases(configuration) {
     const { constructParameters, send, handleError } = require("../services");
-    RED.nodes.createNode(this, configuration);
-    const node = this;
-    this.status({ fill: "green", shape: "dot", text: "Ready" });
     const { client, output } = configuration;
+
+    RED.nodes.createNode(this, configuration);
+
+    const node = this;
+
     node.connection = RED.nodes.getNode(client);
+    node.status({ fill: "blue", shape: "dot", text: "Loading" });
+
+    node.handleEvent = ({ connected, message }) =>
+      node.status(
+        connected
+          ? { fill: "green", shape: "dot", text: message }
+          : { fill: "red", shape: "dot", text: message }
+      );
+
+    node.connection.on("status", node.handleEvent);
+
     node.on("input", async message => {
       this.status({ fill: "yellow", shape: "dot", text: "Processing" });
       const { credentials } = constructParameters(
@@ -15,12 +28,20 @@ module.exports = function(RED) {
         ["credentials"]
       );
 
-      const connection = await this.connection.client;
+      const client = await this.connection.client;
 
-      connection
-        .databases(credentials)
-        .then(response => send(node, output, message, response))
-        .catch(error => handleError(node, error.message, message));
+      client
+        ? client
+            .databases(credentials)
+            .then(response => send(node, output, message, response))
+            .catch(error => handleError(node, error.message, message))
+        : handleError(node, "Failed to load DAPI client.", message);
+    });
+
+    node.on("close", done => {
+      node.connection.removeListener("error", node.handleEvent);
+      node.connection.removeListener("status", node.handleEvent);
+      done();
     });
   }
   RED.nodes.registerType("dapi-databases", databases);
