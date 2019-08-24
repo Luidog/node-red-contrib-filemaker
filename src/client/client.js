740cd79b258c4512a7a739400dc829345e93f4f7
Lui de la Parra <Lui@mutesymphony.com>
2019-08-24 07:47:17 -0600

    Revises Client Node
    This commit revises the client node to emit events upon connection
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/client/client.js
+++ src/client/client.js
@@ -5,125 +5,88 @@ const { connect } = require("marpat");
 function configurationNode(RED) {
   function Client(n) {
     RED.nodes.createNode(this, n);
-    this.status({ fill: "blue", shape: "solid", text: "Loading" });
+    const { concurrency, id, timeout } = n;
     const store = _.get(RED.settings, "marpat.url", "nedb://memory");
     const options = _.get(RED.settings, "marpat.options", {});
+    const connected = () => {
+      if (!this.connected) {
+        this.emit("error", "Client loading error");
+        reject(false);
+      } else {
+        resolve();
+      }
+    };
+
+    this.client = new Promise((resolve, reject) =>
+      setTimeout(connected, "3000")
+    );
     connect(
       store,
       options
-    ).then(db => this.status({ fill: "green", shape: "solid", text: "Ready" }));
-
-    const { concurrency, id, timeout } = n;
-    const configuration = Object.assign(
-      {
-        name: id,
-        concurrency: parseInt(concurrency) || 1,
-        timeout: parseInt(timeout) || 0
-      },
-      {
-        database: this.credentials.database,
-        server: this.credentials.server,
-        user: this.credentials.username,
-        password: this.credentials.password
-      }
-    );
-    this.client = new Promise((resolve, reject) => {
-      !global.CLIENT
-        ? connect(
-            store,
-            options
-          )
-            .then(db => Filemaker.findOne({ name: id }))
-            .then(client => {
-              if (!client) {
-                return Filemaker.create(configuration).save();
-              } else {
-                client.agent.connection.queue = [];
-                client.agent.connection.pending = [];
-                client.agent.connection.starting = false;
-                if (
-                  client.agent.connection.password !== this.credentials.password
-                ) {
-                  client.agent.connection.password = this.credentials.password;
-                  client.agent.connection.sessions = [];
-                }
-                if (client.agent.connection.user !== this.credentials.user) {
-                  client.agent.connection.user = this.credentials.user;
-                  client.agent.connection.sessions = [];
-                }
-                if (
-                  client.agent.connection.server !== this.credentials.server
-                ) {
-                  client.agent.connection.server = this.credentials.server;
-                  client.agent.connection.sessions = [];
-                }
-                if (
-                  client.agent.connection.database !== this.credentials.database
-                ) {
-                  client.agent.connection.database = this.credentials.database;
-                  client.agent.connection.sessions = [];
-                }
-                client.agent.timeout = parseInt(timeout) || 0;
-                client.agent.concurrency = parseInt(concurrency) || 1;
-              }
-
-              return client.save();
-            })
-            .then(client => resolve(client))
-            .catch(error => {
-              this.status({ fill: "red", shape: "dot", text: error.message });
-              reject(error);
-            })
-        : Filemaker.findOne({ name: id })
-            .then(client => {
-              if (!client) {
-                return Filemaker.create(configuration).save();
-              } else {
-                client.agent.connection.queue = [];
-                client.agent.connection.pending = [];
-                if (
-                  client.agent.connection.password !== this.credentials.password
-                ) {
-                  client.agent.connection.password = this.credentials.password;
-                  client.agent.connection.sessions = [];
-                }
-                if (client.agent.connection.user !== this.credentials.user) {
-                  client.agent.connection.user = this.credentials.user;
-                  client.agent.connection.sessions = [];
-                }
-                if (
-                  client.agent.connection.server !== this.credentials.server
-                ) {
-                  client.agent.connection.server = this.credentials.server;
-                  client.agent.connection.sessions = [];
-                }
-                if (
-                  client.agent.connection.database !== this.credentials.database
-                ) {
-                  client.agent.connection.database = this.credentials.database;
-                  client.agent.connection.sessions = [];
-                }
-                client.agent.timeout = parseInt(timeout) || 0;
-                client.agent.concurrency = parseInt(concurrency) || 1;
-              }
-              return client.save();
-            })
-            .then(client => resolve(client))
-            .catch(error => {
-              this.status({ fill: "red", shape: "dot", text: error.message });
-              reject(error);
-            });
-    });
+    )
+      .then(db => {
+        clearTimeout(connected)
+        this.connected = true;
+        const configuration = Object.assign(
+          {
+            name: id,
+            concurrency: parseInt(concurrency) || 1,
+            timeout: parseInt(timeout) || 0
+          },
+          {
+            database: this.credentials.database,
+            server: this.credentials.server,
+            user: this.credentials.username,
+            password: this.credentials.password
+          }
+        );
+        this.client = Filemaker.findOne({ name: id }).then(client => {
+          if (!client) {
+            return Filemaker.create(configuration).save();
+          } else {
+            client.agent.connection.starting = false;
+            client.agent.connection.queue = [];
+            client.agent.connection.pending = [];
+            if (
+              client.agent.connection.password !== this.credentials.password
+            ) {
+              client.agent.connection.password = this.credentials.password;
+              client.agent.connection.sessions = [];
+            }
+            if (client.agent.connection.user !== this.credentials.user) {
+              client.agent.connection.user = this.credentials.user;
+              client.agent.connection.sessions = [];
+            }
+            if (client.agent.connection.server !== this.credentials.server) {
+              client.agent.connection.server = this.credentials.server;
+              client.agent.connection.sessions = [];
+            }
+            if (
+              client.agent.connection.database !== this.credentials.database
+            ) {
+              client.agent.connection.database = this.credentials.database;
+              client.agent.connection.sessions = [];
+            }
+            client.agent.timeout = parseInt(timeout) || 0;
+            client.agent.concurrency = parseInt(concurrency) || 1;
+          }
+          this.emit("status", { connected: true, message: "Ready" });
+          return client.save();
+        });
+      })
+      .catch(({ message }) =>
+        this.emit("status", { connected: false, message })
+      );
 
     this.on("close", function(done) {
+      clearTimeout(connected)
       this.client
         .save()
         .then(client => done())
         .catch(error => {
           this.error(error.message);
-          this.status({ fill: "red", shape: "dot", text: error.message });
+          done();
         });
-      done();
     });
   }
   RED.nodes.registerType("dapi-client", Client, {
