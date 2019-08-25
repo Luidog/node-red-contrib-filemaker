eabf221d7143fcb24f89684f5866255f09913dbe
Lui de la Parra <Lui@mutesymphony.com>
2019-08-25 08:27:18 -0700

    Updates Delete Node
    This commit updates the delete node to properly set node status and protect against an unloaded client.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/nodes/delete.js
+++ src/nodes/delete.js
@@ -1,11 +1,24 @@
 module.exports = function(RED) {
   function deleteRecords(config) {
     const { send, handleError, constructParameters } = require("../services");
-    RED.nodes.createNode(this, config);
-    const node = this;
-    this.status({ fill: "green", shape: "dot", text: "Ready" });
     const { client, output, ...configuration } = config;
+
+    RED.nodes.createNode(this, config);
+
+    const node = this;
+
     node.connection = RED.nodes.getNode(client);
+    node.status({ fill: "blue", shape: "dot", text: "Loading" });
+
+    node.handleEvent = ({ connected, message }) =>
+      node.status(
+        connected
+          ? { fill: "green", shape: "dot", text: message }
+          : { fill: "red", shape: "dot", text: message }
+      );
+
+    node.connection.on("status", node.handleEvent);
+
     node.on("input", async message => {
       this.status({ fill: "yellow", shape: "dot", text: "Processing" });
       const { layout, recordId, ...parameters } = constructParameters(
@@ -14,11 +27,20 @@ module.exports = function(RED) {
         node.context(),
         ["layout", "scripts", "recordId"]
       );
-      const connection = await this.connection.client;
-      connection
-        .delete(layout, recordId, parameters)
-        .then(response => send(node, output, message, response))
-        .catch(error => handleError(node, error.message, message));
+      const client = await this.connection.client;
+
+      client
+        ? client
+            .delete(layout, recordId, parameters)
+            .then(response => send(node, output, message, response))
+            .catch(error => handleError(node, error.message, message))
+        : handleError(node, "Failed to load DAPI client.", message);
+    });
+    
+    node.on("close", done => {
+      node.connection.removeListener("error", node.handleEvent);
+      node.connection.removeListener("status", node.handleEvent);
+      done();
     });
   }
   RED.nodes.registerType("dapi-delete-record", deleteRecords);
