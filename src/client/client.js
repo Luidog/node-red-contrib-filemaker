db4ba23854848eca75ad2f98b97b1941b725b8dc
Lui de la Parra <Lui@mutesymphony.com>
2020-03-08 17:22:00 -0700

    Revises Client Configuration
    This commit revises the client configuration to set the default timeout configuration to 30 seconds.
    
    This commit also surfaces SSL settings to allow non-ssl connections between the client nodes and FIleMaker server.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/client/client.js
+++ src/client/client.js
@@ -6,16 +6,12 @@ function configurationNode(RED) {
   if (!global.MARPAT) global.MARPAT = {};
   function Client(n) {
     RED.nodes.createNode(this, n);
-    const { concurrency, id, timeout } = n;
+    const { concurrency, id, timeout, ssl } = n;
     const store = _.get(RED.settings, "marpat.url", "nedb://memory");
     const options = _.get(RED.settings, "marpat.options", {});
-
     if (!global.MARPAT.CONNECTION) {
       global.MARPAT.CONNECTION = new Promise((resolve, reject) =>
-        connect(
-          store,
-          options
-        )
+        connect(store, options)
           .then(db => {
             this.client = Filemaker.findOne({ name: id })
               .then(client => {
@@ -33,7 +29,8 @@ function configurationNode(RED) {
                         server: this.credentials.server,
                         user: this.credentials.username,
                         password: this.credentials.password
-                      }
+                      },
+                      !ssl ? { agent: { rejectUnauthorized: false } } : {}
                     )
                   ).save();
                 } else {
@@ -66,6 +63,11 @@ function configurationNode(RED) {
                   ) {
                     client.agent.connection.database = this.credentials.database;
                     client.agent.connection.sessions = [];
+                  }
+                  if (!ssl) {
+                    client.agent.agent = { rejectUnauthorized: false };
+                  } else if (client.agent.agent) {
+                    client.agent.agent = { rejectUnauthorized: true };
                   }
                   client.agent.timeout = parseInt(timeout) || 0;
                   client.agent.concurrency = parseInt(concurrency) || 1;
@@ -105,7 +107,8 @@ function configurationNode(RED) {
                     server: this.credentials.server,
                     user: this.credentials.username,
                     password: this.credentials.password
-                  }
+                  },
+                  !ssl ? { agent: { rejectUnauthorized: false } } : {}
                 )
               ).save();
             } else {
@@ -135,6 +138,11 @@ function configurationNode(RED) {
               ) {
                 client.agent.connection.database = this.credentials.database;
                 client.agent.connection.sessions = [];
+              }
+              if (!ssl) {
+                client.agent.agent = { rejectUnauthorized: false };
+              } else if (client.agent.agent) {
+                client.agent.agent = { rejectUnauthorized: true };
               }
               client.agent.timeout = parseInt(timeout) || 0;
               client.agent.concurrency = parseInt(concurrency) || 1;
--- test/client_spec.js
+++ test/client_spec.js
@@ -1905,4 +1905,529 @@ describe("Client Node", function() {
       );
     });
   });
+
+  describe("Client Configuration Tests", function() {
+    beforeEach(function(done) {
+      helper.startServer(done);
+    });
+
+    afterEach(function(done) {
+      helper.settings({});
+      helper.unload();
+      helper.stopServer(() =>
+        setTimeout(() => {
+          delete global.MARPAT;
+          done();
+        }, "500")
+      );
+    });
+
+    it("should create a persistent client without SSL", function(done) {
+      const testFlows = [
+        {
+          id: "ec096890.cdd65",
+          type: "tab",
+          label: "Create Record",
+          disabled: false,
+          info: ""
+        },
+        {
+          id: "369e311d.23d2de",
+          type: "helper"
+        },
+        {
+          id: "d3becaad.b78ce",
+          type: "catch",
+          z: "ec096890.cdd65",
+          name: "",
+          scope: null,
+          x: 360,
+          y: 100,
+          wires: [["369e311d.23d2de"]]
+        },
+        {
+          id: "db596a45.2ca398",
+          type: "dapi-create-record",
+          z: "ec096890.cdd65",
+          client: "e5173483.adc92",
+          layout: "payload.layout",
+          layoutType: "msg",
+          data: "",
+          dataType: "none",
+          scripts: "",
+          scriptsType: "none",
+          merge: "false",
+          mergeType: "bool",
+          output: "payload",
+          x: 340,
+          y: 40,
+          wires: [["369e311d.23d2de"]]
+        },
+        {
+          id: "e5173483.adc92",
+          type: "dapi-client",
+          z: "",
+          name: "Node-RED Test Client",
+          ssl: false,
+          usage: true
+        }
+      ];
+      helper.settings({ marpat: { url: "nedb://dapi" } });
+      helper.load(
+        [clientNode, createNode, catchNode],
+        testFlows,
+        {
+          "e5173483.adc92": {
+            server: process.env.FILEMAKER_SERVER,
+            database: process.env.FILEMAKER_DATABASE,
+            username: process.env.FILEMAKER_USERNAME,
+            password: process.env.FILEMAKER_PASSWORD
+          }
+        },
+        function() {
+          const createNode = helper.getNode("db596a45.2ca398");
+          const helperNode = helper.getNode("369e311d.23d2de");
+          helperNode.on("input", function(msg) {
+            helper.settings({});
+            try {
+              expect(msg)
+                .to.be.an("object")
+                .with.any.keys("payload")
+                .and.property("payload")
+                .to.be.a("object")
+                .with.any.keys("recordId", "modId");
+              done();
+            } catch (err) {
+              done(err);
+            }
+          });
+          createNode.receive({ payload: { layout: "people" } });
+        }
+      );
+    });
+
+    it("should reuse a client without SSL", function(done) {
+      const testFlow = [
+        {
+          id: "ec096890.cdd65",
+          type: "tab",
+          label: "Create Record",
+          disabled: false,
+          info: ""
+        },
+        {
+          id: "369e311d.23d2de",
+          type: "helper"
+        },
+        {
+          id: "d3becaad.b78ce",
+          type: "catch",
+          z: "ec096890.cdd65",
+          name: "",
+          scope: null,
+          x: 360,
+          y: 100,
+          wires: [["369e311d.23d2de"]]
+        },
+        {
+          id: "db596a45.2ca398",
+          type: "dapi-create-record",
+          z: "ec096890.cdd65",
+          client: "e5173483.adc92",
+          layout: "payload.layout",
+          layoutType: "msg",
+          data: "",
+          dataType: "none",
+          scripts: "",
+          scriptsType: "none",
+          merge: "false",
+          mergeType: "bool",
+          output: "payload",
+          x: 340,
+          y: 40,
+          wires: [["369e311d.23d2de"]]
+        },
+        {
+          id: "e5173483.adc92",
+          type: "dapi-client",
+          z: "",
+          name: "Node-RED Test Client",
+          ssl: false,
+          usage: true
+        }
+      ];
+      helper.settings({ marpat: { url: "nedb://dapi" } });
+      helper.load(
+        [clientNode, createNode, catchNode],
+        testFlow,
+        {
+          "e5173483.adc92": {
+            server: process.env.FILEMAKER_SERVER,
+            database: process.env.FILEMAKER_DATABASE,
+            username: process.env.FILEMAKER_USERNAME,
+            password: process.env.FILEMAKER_PASSWORD
+          }
+        },
+        function() {
+          const createNode = helper.getNode("db596a45.2ca398");
+          const helperNode = helper.getNode("369e311d.23d2de");
+          helperNode.on("input", function(msg) {
+            try {
+              expect(msg)
+                .to.be.an("object")
+                .with.any.keys("payload")
+                .and.property("payload")
+                .to.be.a("object")
+                .with.any.keys("recordId", "modId");
+              done();
+            } catch (err) {
+              done(err);
+            }
+          });
+          createNode.receive({ payload: { layout: "people" } });
+        }
+      );
+    });
+
+    it("should allow a client's server configuration to be modified without SSL", function(done) {
+      const testFlow = [
+        {
+          id: "ec096890.cdd65",
+          type: "tab",
+          label: "Create Record",
+          disabled: false,
+          info: ""
+        },
+        {
+          id: "369e311d.23d2de",
+          type: "helper"
+        },
+        {
+          id: "d3becaad.b78ce",
+          type: "catch",
+          z: "ec096890.cdd65",
+          name: "",
+          scope: null,
+          x: 360,
+          y: 100,
+          wires: [["369e311d.23d2de"]]
+        },
+        {
+          id: "db596a45.2ca398",
+          type: "dapi-create-record",
+          z: "ec096890.cdd65",
+          client: "e5173483.adc92",
+          layout: "payload.layout",
+          layoutType: "msg",
+          data: "",
+          dataType: "none",
+          scripts: "",
+          scriptsType: "none",
+          merge: "false",
+          mergeType: "bool",
+          output: "payload",
+          x: 340,
+          y: 40,
+          wires: [["369e311d.23d2de"]]
+        },
+        {
+          id: "e5173483.adc92",
+          type: "dapi-client",
+          z: "",
+          name: "Node-RED Test Client",
+          ssl: false,
+          usage: true
+        }
+      ];
+      helper.settings({ marpat: { url: "nedb://dapi" } });
+      helper.load(
+        [clientNode, createNode, catchNode],
+        testFlow,
+        {
+          "e5173483.adc92": {
+            server: "https://updated.server.com",
+            database: process.env.FILEMAKER_DATABASE,
+            username: process.env.FILEMAKER_USERNAME,
+            password: process.env.FILEMAKER_PASSWORD
+          }
+        },
+        function() {
+          const client = helper.getNode("e5173483.adc92");
+          try {
+            expect(client)
+              .to.be.an("object")
+              .with.any.keys("id", "type", "name", "credentials", "connection")
+              .and.property("credentials")
+              .to.be.an("object")
+              .with.any.keys("server", "database", "username", "password")
+              .and.property("server")
+              .to.eql("https://updated.server.com");
+            done();
+          } catch (err) {
+            done(err);
+          }
+        }
+      );
+    });
+
+    it("should allow a client's database configuration to be modified without SSL", function(done) {
+      const testFlow = [
+        {
+          id: "ec096890.cdd65",
+          type: "tab",
+          label: "Create Record",
+          disabled: false,
+          info: ""
+        },
+        {
+          id: "369e311d.23d2de",
+          type: "helper"
+        },
+        {
+          id: "d3becaad.b78ce",
+          type: "catch",
+          z: "ec096890.cdd65",
+          name: "",
+          scope: null,
+          x: 360,
+          y: 100,
+          wires: [["369e311d.23d2de"]]
+        },
+        {
+          id: "db596a45.2ca398",
+          type: "dapi-create-record",
+          z: "ec096890.cdd65",
+          client: "e5173483.adc92",
+          layout: "payload.layout",
+          layoutType: "msg",
+          data: "",
+          dataType: "none",
+          scripts: "",
+          scriptsType: "none",
+          merge: "false",
+          mergeType: "bool",
+          output: "payload",
+          x: 340,
+          y: 40,
+          wires: [["369e311d.23d2de"]]
+        },
+        {
+          id: "e5173483.adc92",
+          type: "dapi-client",
+          z: "",
+          name: "Node-RED Test Client",
+          ssl: false,
+          usage: true
+        }
+      ];
+      helper.settings({ marpat: { url: "nedb://dapi" } });
+      helper.load(
+        [clientNode, createNode, catchNode],
+        testFlow,
+        {
+          "e5173483.adc92": {
+            server: process.env.FILEMAKER_SERVER,
+            database: "updated-database",
+            username: process.env.FILEMAKER_USERNAME,
+            password: process.env.FILEMAKER_PASSWORD
+          }
+        },
+        function() {
+          const client = helper.getNode("e5173483.adc92");
+          try {
+            expect(client)
+              .to.be.an("object")
+              .with.any.keys("id", "type", "name", "credentials", "connection")
+              .and.property("credentials")
+              .to.be.an("object")
+              .with.any.keys("server", "database", "username", "password")
+              .and.property("database")
+              .to.eql("updated-database");
+            done();
+          } catch (err) {
+            done(err);
+          }
+        }
+      );
+    });
+    it("should allow a client's SSL configuration to be modified", function(done) {
+      const testFlow = [
+        {
+          id: "ec096890.cdd65",
+          type: "tab",
+          label: "Create Record",
+          disabled: false,
+          info: ""
+        },
+        {
+          id: "369e311d.23d2de",
+          type: "helper"
+        },
+        {
+          id: "d3becaad.b78ce",
+          type: "catch",
+          z: "ec096890.cdd65",
+          name: "",
+          scope: null,
+          x: 360,
+          y: 100,
+          wires: [["369e311d.23d2de"]]
+        },
+        {
+          id: "db596a45.2ca398",
+          type: "dapi-create-record",
+          z: "ec096890.cdd65",
+          client: "e5173483.adc92",
+          layout: "payload.layout",
+          layoutType: "msg",
+          data: "",
+          dataType: "none",
+          scripts: "",
+          scriptsType: "none",
+          merge: "false",
+          mergeType: "bool",
+          output: "payload",
+          x: 340,
+          y: 40,
+          wires: [["369e311d.23d2de"]]
+        },
+        {
+          id: "e5173483.adc92",
+          type: "dapi-client",
+          z: "",
+          name: "Node-RED Test Client",
+          ssl: false,
+          usage: true
+        }
+      ];
+      helper.settings({ marpat: { url: "nedb://dapi" } });
+      helper.load(
+        [clientNode, createNode, catchNode],
+        testFlow,
+        {
+          "e5173483.adc92": {
+            server: process.env.FILEMAKER_SERVER,
+            database: process.env.FILEMAKER_DATABASE,
+            username: "updated-username",
+            password: process.env.FILEMAKER_PASSWORD
+          }
+        },
+        function() {
+          const client = helper.getNode("e5173483.adc92");
+          try {
+            expect(client)
+              .to.be.an("object")
+              .with.any.keys("id", "type", "name", "credentials","connection");
+            done();
+          } catch (err) {
+            done(err);
+          }
+        }
+      );
+    });
+
+    it("should create a client of one does not exist", function(done) {
+      const testFlow = [
+        {
+          id: "37b558cf.8553c",
+          type: "tab",
+          label: "Perform Find",
+          disabled: false,
+          info: ""
+        },
+        {
+          id: "641ec5c9.c3f73c",
+          type: "catch",
+          z: "37b558cf.8553c",
+          name: "",
+          scope: null,
+          x: 260,
+          y: 100,
+          wires: [["cfc785d0.b2ba"]]
+        },
+        {
+          id: "cfc785d0.b2ba",
+          type: "helper"
+        },
+        {
+          id: "f15d2a17.bbc65",
+          type: "dapi-perform-find",
+          z: "37b558cf.8553c",
+          client: "e5173483.adc92",
+          layout: "payload.layout",
+          layoutType: "msg",
+          limit: "",
+          limitType: "num",
+          offset: "",
+          offsetType: "num",
+          sort: "",
+          sortType: "none",
+          query: "payload.query",
+          queryType: "msg",
+          scripts: "",
+          scriptsType: "none",
+          portals: "",
+          portalsType: "none",
+          output: "payload",
+          x: 250,
+          y: 40,
+          wires: [["cfc785d0.b2ba"]]
+        },
+        {
+          id: "e5173483.adc92",
+          type: "dapi-client",
+          z: "",
+          name: "Node-RED Test Client",
+          usage: true
+        },
+        {
+          id: "e5173483.adc91",
+          type: "dapi-client",
+          z: "",
+          name: "Node-RED Test Client",
+          usage: true
+        }
+      ];
+      helper.settings({ marpat: { url: "nedb://clients" } });
+      helper.load(
+        [clientNode, findNode, catchNode],
+        testFlow,
+        {
+          "e5173483.adc92": {
+            server: process.env.FILEMAKER_SERVER,
+            username: process.env.FILEMAKER_USERNAME,
+            password: process.env.FILEMAKER_PASSWORD
+          },
+          "e5173483.adc91": {
+            server: process.env.FILEMAKER_SERVER,
+            username: process.env.FILEMAKER_USERNAME,
+            password: process.env.FILEMAKER_PASSWORD
+          }
+        },
+        function() {
+          const findNode = helper.getNode("f15d2a17.bbc65");
+          const helperNode = helper.getNode("cfc785d0.b2ba");
+
+          helperNode.on("input", function(msg) {
+            try {
+              expect(msg)
+                .to.be.an("object")
+                .with.any.keys("payload", "error", "_msgid")
+                .and.property("error")
+                .to.be.a("object")
+                .with.any.keys("message", "source")
+                .and.property("message")
+                .to.be.a("string");
+              done();
+            } catch (err) {
+              done(err);
+            }
+          });
+          findNode.receive({
+            payload: { layout: "people", query: { name: "*" } }
+          });
+        }
+      );
+    });
+  });
 });
